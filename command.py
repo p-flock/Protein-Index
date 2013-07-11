b10d8f72153a48d7429cd135d9f770cdf8e51995
p-flock <peter.flockhart@gmail.com>
2013-07-11 12:52:36 -0400

    Fixed function returning function weirdness

--- command.py
+++ command.py
@@ -92,7 +92,7 @@ def scrape_data_final(url):
 	return result
 
 def match_class(target):                                                        
-    def do_match(tag):                                                          
+#    def do_match(tag):                                                          
         classes = tag.get('class', [])                                          
         return all(c in classes for c in target)                                
-    return do_match                                 
+#    return do_match                                 
